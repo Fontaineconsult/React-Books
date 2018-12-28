52af4da99731749492427ea6390e74465d166bfe
Daniel Fontaine <fontaine@sfsu.edu>
2018-12-28 11:40:01 -0800

    Changed change shelf comp to a be a controlled component. Value of select is now controlled by state and is updated from app component

--- src/components/ChangeShelfComponent.js
+++ src/components/ChangeShelfComponent.js
@@ -3,19 +3,43 @@ import React from 'react'
 class ChangeShelf extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {currentShelf: ""};
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+
+    handleChange(event) {
+
+        this.setState({currentShelf: event.target.value})
 
     }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        console.log(event)
+        this.props.changeShelf(event)
+    }
+
+    componentWillMount() {
+        console.log("Mounted", this.props.book.shelf)
+        this.setState({currentShelf: this.props.book.shelf})
+
+    }
+
+
 
     render() {
 
         return(
 
             <div className="change-submit-form" onMouseLeave={this.props.showOrHide}>
-                <form className="change-submit-form" onSubmit={this.props.changeShelf}>
+                <form className="change-submit-form" onSubmit={this.handleSubmit}>
                     <input name="CurShelf" type="hidden" value={this.props.book.shelf}/>
                     <input name="BookID" type="hidden" value={this.props.book.id}/>
-                    <select size="3" className="change-submit-form" name="shelfSelect">
+                    <select size="4" className="change-submit-form" value={this.state.currentShelf} name="shelfSelect" onChange={this.handleChange}>
+                        <option value="move" disabled>Move to...</option>
                         <option value="read">Already Read</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
